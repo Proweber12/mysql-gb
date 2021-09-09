ce8a68b854724de028c17ed757c80d2d93d1dff3
Mihail <borisonchick@gmail.com>
2021-09-09 14:54:14 +0300

    did homework-2

---
+++ hw2.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS example;
+CREATE DATABASE example;
+USE example;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255) DEFAULT 'anonymus'
+);
