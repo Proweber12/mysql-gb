2226fa9e7625ed749d0ed95fa862b9424e899592
Mihail <borisonchick@gmail.com>
2021-10-14 19:46:22 +0300

    Added file architecture

--- Администрирование/hw1.sql
+++
@@ -1,9 +0,0 @@
-drop user if exists shop_read;
-create user shop_read;
-GRANT SELECT ON shop.* TO 'shop_read'@'%';
-
-drop user if exists shop;
-create user shop;
-GRANT ALL PRIVILEGES ON shop.* TO 'shop'@'%';
-
-FLUSH PRIVILEGES;
